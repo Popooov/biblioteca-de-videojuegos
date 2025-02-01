50c390e80cce6c508b03cf3b3877223ff8ebcf7b
Oleksandr <31711254+Popooov@users.noreply.github.com>
2025-02-01 02:08:04 +0000

    Añadir controladores para registro e inicio de sesión, rutas, componentes para formulario

--- resources/views/videojuegos/create.blade.php
+++ resources/views/videojuegos/create.blade.php
@@ -7,69 +7,48 @@
         <div class="space-y-12">
           <div class="border-b border-gray-900/10 pb-12">
             <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div class="sm:col-span-4">
+              
+              <x-form-field>
                 <x-form-label for="titulo">Titulo</x-form-label>
                 <div class="mt-2">
-                  <x-form-input name="titulo" id="titulo" placeholder="titulo de videojuego"  required />
+                  <x-form-input name="titulo" id="titulo" placeholder="titulo de videojuego" required />
                   <x-form-error name="titulo" />
                 </div>
-              </div>
-
-              <div class="sm:col-span-4">
-                <label for="lanzamiento" class="block text-sm/6 font-medium text-gray-900">Lanzamiento</label>
+              </x-form-field>
+              
+              <x-form-field>
+                <x-form-label for="lanzamiento">Lanzamiento</x-form-label>
                 <div class="mt-2">
-                  <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="lanzamiento" id="lanzamiento" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" required placeholder="lanzamiento de videojuego">
-                  </div>
-
-                  @error('lanzamiento')
-                    <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                  @enderror
-
+                  <x-form-input name="lanzamiento" id="lanzamiento" placeholder="lanzamiento de videojuego" required />
+                    <x-form-error name="lanzamiento" />
                 </div>
-              </div>
-
-              <div class="sm:col-span-4">
-                <label for="genero" class="block text-sm/6 font-medium text-gray-900">Genero</label>
+              </x-form-field>
+              
+              <x-form-field>
+                <x-form-label for="genero">Genero</x-form-label>
                 <div class="mt-2">
-                  <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="genero" id="genero" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" required placeholder="genero de videojuego">
-                  </div>
-
-                  @error('genero')
-                    <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                  @enderror
-
+                  <x-form-input name="genero" id="genero" placeholder="genero de videojuego" required />
+                    <x-form-error name="genero" />
                 </div>
-              </div>
-
-              <div class="sm:col-span-4">
-                <label for="plataforma" class="block text-sm/6 font-medium text-gray-900">Plataforma</label>
+              </x-form-field>
+              
+              <x-form-field>
+                <x-form-label for="plataforma">Plataforma</x-form-label>
                 <div class="mt-2">
-                  <div class="flex items-center rounded-md bg-white pl-3 outline outline-1 -outline-offset-1 outline-gray-300 focus-within:outline focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-                    <input type="text" name="plataforma" id="plataforma" class="block min-w-0 grow py-1.5 pl-1 pr-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6" required placeholder="plataforma de videojuego">
-                  </div>
-
-                  @error('plataforma')
-                    <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                  @enderror
-
+                  <x-form-input name="plataforma" id="plataforma" placeholder="plataforma de videojuego" required />
+                  <x-form-error name="plataforma" />
                 </div>
-              </div>
-      
-              <div class="col-span-full mb-5">
-                <label for="descripcion" class="block text-sm/6 font-medium text-gray-900">Descripción</label>
+              </x-form-field>
+              
+              <x-form-field>
+                <x-form-label for="descripcion">Descripcion</x-form-label>
                 <div class="mt-2">
-                  <textarea name="descripcion" id="descripcion" rows="3" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" required></textarea>
+                  <x-form-textarea name="descripcion" id="descripcion" rows="3" placeholder="descripcion de videojuego" required />
+                  <x-form-error name="descripcion" />
                 </div>
-
-                @error('descripcion')
-                  <p class="text-xs text-red-600 font-semibold mt-1">{{ $message }}</p>
-                @enderror
-
-              </div>
+              </x-form-field>
             </div>
-
+            
             <div class="col-span-full">
                 <label for="cover-photo" class="block text-sm/6 font-medium text-gray-900">Imagen de videojuego</label>
                 <div class="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
@@ -95,8 +74,7 @@
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
           <a href="/" class="text-sm/6 font-semibold text-gray-700">Cancelar</a>
-          <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Añadir</button>
+          <x-form-button>Añadir</x-form-button>
         </div>
       </form>
-      
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\VideojuegoController;
 use Illuminate\Support\Facades\Route;
 
@@ -12,6 +14,13 @@ Route::controller(VideojuegoController::class)->group(function () {
     Route::patch('/{videojuego}', 'update');
     Route::delete('/{videojuego}', 'destroy');
 });
+
+// Auth
+Route::get('/u/register', [RegisteredUserController::class, 'create']);
+Route::post('/u/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/u/login', [SessionController::class, 'create']);
+Route::post('/u/login', [SessionController::class, 'store']);
 
 // Route::get('/', function () {
 //     return redirect('/videojuegos');
